8e76138f5c377d75ad114647a4b17e1caf525e64
shweta <shwetamathanker1203@gmail.com>
2024-04-12 19:40:53 +0530

    react frontend

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+
+const 
+gr = () => {
+  return (
+    <div>
+       <div className="nav">
+        <div className="text">
+          <h3>New swimwear with Lime Ricki out now!</h3>
+        </div>
+        <div className="links">
+          <h3>ABOUT</h3>
+          <h3>PARTNERSHIP</h3>
+          <h3>PRESS</h3>
+          <h3>SUBSCRIBE</h3>
+          <h3>BOOKS</h3>
+          <h3>COURSES</h3>
+          <h3>OUR SHOP</h3>
+        </div>
+       </div>
+      <div className="navdown">
+        <div className="logopart">
+        <i className="menuicon ri-menu-line"></i> <img className='imglogo'  src="https://thehousethatlarsbuilt.com/wp-content/uploads/2023/06/cropped-lars-logo.png" alt="" />
+                  </div>
+        <div className="linkagain">
+          <h3>HOLIDAY</h3>
+          <h3>DIY & CRAFTS</h3>
+          <h3>DECOR & INTERIORS</h3>
+          <h3>STYLE</h3>
+          <h3>FOOD & ENTERTAINING</h3>
+          <h3>LIFESTYLE</h3>
+          <h3>THINGS WE LOVE</h3>
+        </div>
+        <div className="search">
+          <h3>Search </h3><i className="ri-search-line"></i>
+        </div>
+      </div>
+ <div className="centerdiv">
+ <div className="center">
+<img className='imgcenter' src=" https://thehousethatlarsbuilt.com/wp-content/uploads/2024/03/EasterEggCakes_WEB_13-960x1160.jpg" alt="" />
+ </div>
+ <div className="middlecenter">
+  <img className='imgcenter' src="https://thehousethatlarsbuilt.com/wp-content/uploads/2023/07/Spoonflower_EastForkPottery_9-e1689282648705-960x1160.jpg" alt="" />
+ </div>
+ <div className='titlecenter'>
+  <h2 className='text-red-700 text-[2vw] translate-x-[35%]'>HOLIDAY</h2>
+<h1 className='text-[3vw] leading-[3vw] text-center font-[cursive]' > Spring Tablescape <br /> ideas </h1>
+ </div>
+ <div className="lastcenter">
+  <img className='imgcenter' src="https://thehousethatlarsbuilt.com/wp-content/uploads/2016/04/block_print_tulip_napkin_DIY_13-e1461252627974.jpg" alt="" />
+ </div>
+ </div>
+ 
+    </div>
+  )
+}
+
+export default 
+gr
